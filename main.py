5acc354a99b2dec35f13dc2b6f94ca67c3446386
musu <anamikapal932@gmail.com>
2026-08-08 11:13:26 +0530

    create basic route and get requests

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# home route.
 @app.get("/")
 def home():
-    return {"message":"HELLO WORLD with fastapi with venv"}
+    return {"message":"HELLO WORLD"}
+
+# about route.
+@app.get("/about")
+def about():
+    return {"message":"This is a about page."}
+
+# user route.
+@app.get("/user")
+def user():
+    return {"users":
+            ["mohit","rohit","nandini"]
+            }

28435bcaad1d69663e2286956ef82de817a8a5cf
musu <anamikapal932@gmail.com>
2026-08-10 12:06:49 +0530

    Request Body

--- main.py
+++ main.py
@@ -1,35 +1,18 @@
-from fastapi import FastAPI , Query
+from fastapi import FastAPI 
 from typing import Optional
+from pydantic import BaseModel, Field
+
+class User(BaseModel):
+    name:str = Field(min_length=3, max_length=20)
+    age:int = Field(1, ge=18)
+    objective:Optional[str] = Field(None,min_length=3, max_length=20)
 
 app = FastAPI()
+ # real life example.
+@app.post("/create-user")
+def create_user(user: User):
+    return{
+       "msg : User has created."
+       "user" : user
+    }
 
-# home route.
-@app.get("/")
-def home():
-    return {"message":"HELLO WORLD"}
-
-# about route.
-@app.get("/about")
-def about():
-    return {"message":"This is a about page."}
-
-# user route.
-# @app.get("/user")
-# def get_user(name:str=None):
-#     return {"name":
-#             name
-#             }
-
-
-@app.get("/user")
-def get_user_read(item_name:Optional[str]= Query(None, max_length=50, min_length=3)
-             ,limit:int=Query(1,ge=1),
-              price:int=Query(50,le=1000) ):
-    
-    return {"limit":
-            limit,
-            "item_name":
-            item_name,
-            "price":
-            price
-            }

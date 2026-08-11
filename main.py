1d49019fa5b600b33fecc597b3c023a756d6c048
musu <anamikapal932@gmail.com>
2026-08-11 06:29:30 +0530

    Scemas and pydantic topic

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 from fastapi import FastAPI 
-from typing import Optional
-from pydantic import BaseModel, Field
-
-class User(BaseModel):
-    name:str = Field(min_length=3, max_length=20)
-    age:int = Field(1, ge=18)
-    objective:Optional[str] = Field(None,min_length=3, max_length=20)
+# from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
- # real life example.
+
+# class User(BaseModel):
+#     name:str
+#     age:int
+#     email:str
+
+# @app.post("/create-user")
+# def create_user(user:User):
+#     return {
+#         "msg": "user created",
+#         "user": user
+#     }
+
+class Address(BaseModel):
+    city:str
+    pincode:int
+
+class User(BaseModel):
+    name: str
+    age : int
+    address: Address
+
 @app.post("/create-user")
-def create_user(user: User):
-    return{
-       "msg : User has created."
-       "user" : user
-    }
+def create_user(user:User):
+    return user
+
+
+
+    
+
+
 

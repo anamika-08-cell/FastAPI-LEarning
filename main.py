12190fc7d7d693c20f15ea40cab8c18b88b2ffa1
musu <anamikapal932@gmail.com>
2026-08-14 08:26:46 +0530

    Basic exception handling

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
-from fastapi import FastAPI 
-from pydantic import BaseModel
+from fastapi import FastAPI ,status, HTTPException
 
 app = FastAPI()
 
-# users = [ ]
-
-class User(BaseModel):
-    name:str
-    age:int
-    password:str
-
-class ResponseUser(BaseModel):
-    name:str
-    age:int
-
-@app.get("/users",response_model=ResponseUser) 
-def create_users():
+@app.post("/create_user", status_code=status.HTTP_201_CREATED)
+def create_user():
     return{
-        "name":"A",
-        "age":17,
-        "password":123456
+        "User has Created!!"
+    }
+
+@app.get("/create-users")
+def create_users():
+
+    return{
+        "status": 200,
+        "msg":"user fetched",
+        "data":{
+            "name":"mohit",
+            "age":15
+        }
+    }
+
+@app.get("/user/{user_id}")
+def get_user(user_id:int):
+    if user_id != 1:
+        raise HTTPException(
+            status_code = 404,
+            detail = "user has not fetched successfully."
+        )
+    return {
+        "id" : 1,
+        "name" :"mohit",
+        "age": 10
+
+    }
+            
         
-    }   #be careful.
     
 
 

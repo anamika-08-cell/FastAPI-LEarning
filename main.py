a4e78f818f178dd217d7e17e523945356f6f4472
musu <anamikapal932@gmail.com>
2026-08-16 09:38:50 +0530

    dependency injection

--- main.py
+++ main.py
@@ -1,25 +1,49 @@
-from fastapi import FastAPI, HTTPException,Request
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Depends, Header,HTTPException
+
 app = FastAPI()
-class UserNotFoundException(Exception):
-    def __init__(self, name):
-        self.name = name
-@app.exception_handler(UserNotFoundException)
-def user_not_found_exception(request : Request, exc : UserNotFoundException):
-    return JSONResponse(
-        status_code=404,
-        content={
-            "status":"error",
-            "msg":f"user not found {exc.name}."
-        }
-    )
-@app.get("/create_user")
-def create_user(name):
-    if name != "mohit":
-        raise UserNotFoundException(name)
+
+#auth example
+def verify_token(token : str = Header(None)):
+    if token != "mysecrettoken":
+        raise HTTPException(
+            status_code = 401,
+            detail = {
+                "msg": "unauthorized user !"
+            }
+        )
     return {
-        "name":name
+        "msg":"secure authorized access."
     }
+
+@app.get("/secuire-user")
+def secure_user(user = Depends(verify_token)):
+    return user
+
+
+
+# def common_logic():
+#     return{
+#         "msg":"common logic executed."
+#     }
+
+# @app.get("/home")
+# def user(data = Depends(common_logic)):
+#     return data
+
+# def get_currrent_user():
+#     return {
+#         "msg":"Mohit"
+#     }
+
+# @app.get("/profile")
+# def users(user = Depends(get_currrent_user)):
+#     return user
+
+# @app.get("/dashboard")
+# def dashboard(dashboards = Depends(get_currrent_user)):
+#     return dashboards
+
+
 
 
 

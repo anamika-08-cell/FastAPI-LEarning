110c8c02ebdd4de52c1ab8852c8f8a339c3bac88
musu <anamikapal932@gmail.com>
2026-08-17 09:11:49 +0530

    SQLAlchemy Setup and connect with fastapi

---
+++ main.py
@@ -0,0 +1,43 @@
+from sqlalchemy import create_engine, Column,Integer,String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from fastapi import FastAPI, Depends
+
+app = FastAPI()
+DATABASE_URL = "sqlite:///./test.db"
+
+#db connect kia
+engine = create_engine(
+    DATABASE_URL,
+    connect_args = {
+        "check_same_thread":False
+    }
+)
+
+#db ke through operation kr sakte ho.
+sessionLocal = sessionmaker(bind=engine)
+
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__= "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    completed = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+#fastapi ko db se connect karte hai.
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#test api banate hai.
+@app.get("/")    
+def home(db: Session = Depends(get_db)):
+    return{
+        "msg":"DB connected fine."
+    }  

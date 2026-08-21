ddd354e78caf8a7c54c980bd819156cb2f386516
musu <anamikapal932@gmail.com>
2026-08-21 09:50:45 +0530

    crud operation with swagger UI and BD

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from sqlalchemy import create_engine, Column,Integer,String
+from sqlalchemy import create_engine, Column,Integer,String, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from fastapi import FastAPI, Depends
 
 app = FastAPI()
+
+#database file kaha banegi use batane ke liye.
 DATABASE_URL = "sqlite:///./test.db"
 
-#db connect kia
+#db connect kia.
 engine = create_engine(
     DATABASE_URL,
     connect_args = {
@@ -13,11 +15,13 @@ engine = create_engine(
     }
 )
 
-#db ke through operation kr sakte ho.
+#session actual data type hai.
+#db ke through operation kr sakte ho. har ek request ke liye session create krta hai.
 sessionLocal = sessionmaker(bind=engine)
-
+#base model.
 Base = declarative_base()
 
+#create model class.
 class Todo(Base):
     __tablename__= "todos"
 
@@ -27,7 +31,7 @@ class Todo(Base):
 
 Base.metadata.create_all(bind=engine)
 
-#fastapi ko db se connect karte hai.
+#fastapi ko db se connect karte hai database session hai ye.
 def get_db():
     db = sessionLocal()
     try:
@@ -35,9 +39,14 @@ def get_db():
     finally:
         db.close()
 
-#test api banate hai.
-@app.get("/")    
-def home(db: Session = Depends(get_db)):
-    return{
-        "msg":"DB connected fine."
-    }  
+#create post api.
+@app.post("/todos")
+def create_todo(title:str,db:Session=Depends(get_db)):
+    todo = Todo(title=title,completed=False)
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return {
+       "message":"Todo Created",
+       "data": todo 
+    }
